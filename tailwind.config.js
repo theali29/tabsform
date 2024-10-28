169fd64c6131d20e24bf01b96148e5ffca8478d0
theali29 <muhammad.786685@gmail.com>
2024-10-28 17:28:28 +0500

    Your commit message here

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // This includes all your React files
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pacifico: ['Pacifico', 'cursive'],
+        merriweather: ['Merriweather', 'serif'],
+        apercupro: [
+          'apercu-pro',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Helvetica Neue"',
+          'sans-serif',
+        ],
+        libre: ['Libre Baskerville', 'serif'],
+      },
+      gridColumn: {
+        '3-span-2': '3 / span 2', // Custom utility
+        '6-span-2': ' 6 / span 2',
+        '9-span-2': ' 9 / span 2',
+      },
+    },
+  },
+  plugins: [],
+}
