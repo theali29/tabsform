015a325482974f235009eeb66f8e346c73e7b28e
theali29 <muhammad.786685@gmail.com>
2024-12-09 19:51:12 +0500

    Updated changes

--- src/pages/product.js
+++ src/pages/product.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 function Product() {
   return (
     <div>
-      <h1>Welcome to the products</h1>
+      <h1>Welcome to the products.</h1>
     </div>
   )
 }
