6b2677f063d2c132cf1570f08f2278d5a4300fa1
theali29 <muhammad.786685@gmail.com>
2024-12-31 23:28:10 +0500

    Update product page styling

--- src/pages/product.js
+++ src/pages/product.js
@@ -8,7 +8,7 @@ export default function Product() {
           <div>
             <h1>Start with better forms. End with better output.</h1>
           </div>
-          <div className='text-center text-2xl items-center text-[rgb(25,25,25)] pt-4 '>
+          <div className='text-center text-xl items-center text-[rgb(25,25,25)] pt-4 '>
             <p>
               We are a team of developers, designers, and marketers who are
               passionate about creating better forms.
