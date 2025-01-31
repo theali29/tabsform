1f3331d6487e7643a7adbfc86061904aba973344
theali29 <muhammad.786685@gmail.com>
2025-02-01 02:30:07 +0500

    Build issues

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -6,6 +6,7 @@ import {
   signInWithPopup,
   signInWithRedirect,
   getRedirectResult,
+  signInWithEmailAndPassword
 } from 'firebase/auth'
 import { auth } from '../Firebase/firebase.config'
 
@@ -66,6 +67,16 @@ export default function SignUp() {
       console.error('Sign-in error:', error.message)
     }
   }
+  const signInWithEmailAndPassword = async () => {
+  signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+    // Signed in
+    const user = userCredential.user
+    // ...
+  }).catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+    // ..
+  })
 
   return (
     <div>
