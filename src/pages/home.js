f5c5b1e3127f0ed3ef51d759a0719d668e0465e4
theali29 <muhammad.786685@gmail.com>
2025-02-14 03:38:16 +0500

    Added content on Product page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,48 +1,13 @@
 import '../App.css'
 import video from '../video/trailer1.mp4'
-import airbnb from '../images/airbnb-logo.svg'
-import loewe from '../images/Loewe.svg'
-import sugarcrm from '../images/sugarcrm.svg'
-import klaviyo from '../images/Klaviyo.svg'
-import cartier from '../images/Cartier.svg'
 import image from '../images/image2.png'
 import { Link } from 'react-router-dom'
 // import Product from './product.js'
 // import SignUp from './signup.js'
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 //import Layout from '../components/Layout.js'
+import BrandCarousel from '../components/brandCarousel'
 export default function Home() {
-  const [isMobile, setIsMobile] = useState(false)
-  const [activeIndex, setActiveIndex] = useState(0)
-
-  const logos = [
-    { src: airbnb, alt: 'Loewe logo' },
-    { src: loewe, alt: 'Airbnb logo' },
-    { src: sugarcrm, alt: 'SugarCRM logo' },
-    { src: klaviyo, alt: 'Klaviyo logo' },
-    { src: cartier, alt: 'Cartier logo' },
-  ]
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768) // Set breakpoint for mobile
-    }
-
-    handleResize()
-    window.addEventListener('resize', handleResize)
-
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
-  useEffect(() => {
-    if (isMobile) {
-      const interval = setInterval(() => {
-        setActiveIndex((prevIndex) => (prevIndex + 1) % logos.length) // Loop through logos
-      }, 1000) // Adjust the speed of transition (1.5 seconds)
-
-      return () => clearInterval(interval)
-    }
-  }, [isMobile, logos.length])
   return (
     <>
       <div className='min-w-screen max-w-full overflow-x-hidden'>
@@ -89,54 +54,7 @@ export default function Home() {
                 </div>
               </div>
             </section>
-            <section className='px-4 py-16 grid m-auto'>
-              {/* Desktop View */}
-              {!isMobile && (
-                <section className='px-4 py-16 grid m-auto grid-cols-[repeat(4,1fr)] gap-4 xxs:gap-6 xxs:grid-cols-[repeat(8,1fr)] lg:grid-cols-[repeat(12,1fr)] lg:gap-6'>
-                  <div className='col-start-1 -col-end-1'>
-                    <div className='grid gap-y-0 gap-x-6 grid-cols-[repeat(4,1fr)] justify-center my-0 -mx-4 xxs:grid-cols-[repeat(4,minmax(56px, 64px))] md:grid-cols-[repeat(8,minmax(56px, 64px))]  md:max-md:overflow-hidden text-center lg:grid-cols-[repeat(12,minmax(56px, 64px))] xl:grid-cols-[repeat(12,64px)] xl:gap-y-0 xl:gap-x-8 '>
-                      <div className='col-start-1 -col-end-1 w-full gap-y-6 lg:px-16 lg:py-0 lg:justify-between md:flex md:flex-wrap  md:justify-center md:max-md:overflow-hidden'>
-                        {logos.map((logo, idx) => (
-                          <div key={idx} className='flex justify-center'>
-                            <img
-                              src={logo.src}
-                              alt={logo.alt}
-                              className='mx-4'
-                            />
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                </section>
-              )}
-
-              {/* Mobile View */}
-              {isMobile && (
-                <div className='relative overflow-hidden h-20'>
-                  <div
-                    className='absolute flex transition-transform ease-in-out duration-500'
-                    style={{
-                      transform: `translateX(-${activeIndex * 100}%)`,
-                    }}
-                  >
-                    {logos.map((logo, idx) => (
-                      <div
-                        key={idx}
-                        className='w-full flex-shrink-0 text-sm flex justify-center'
-                      >
-                        <img
-                          src={logo.src}
-                          alt={logo.alt}
-                          className='mx-auto w-24'
-                        />
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              )}
-            </section>
-
+            <BrandCarousel></BrandCarousel>
             {/* <section className='px-4 py-16 grid m-auto'>
               <div className='col-start-1 -col-end-1'>
                 <div className='flex justify-center md:flex-wrap md:gap-8 transition-transform ease-in-out'>
--- src/pages/product.js
+++ src/pages/product.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import BrandCarousel from '../components/brandCarousel'
 export default function Product() {
   return (
     <div className='bg-white pt-[120px]'>
       <div className='container mx-auto px-4 py-8 '>
-        <div className='text-center col-start-1 -col-end-1 text-xl lg:col-start-3 lg:-col-end-3 md:col-start-2 md:-col-end-2 flex flex-col grid-rows-1 justify-center  items-center text-[rgb(25,25,25)] pt-4 '>
-          <h1 className='text-5xl xl:text-[74px] xl:leading-[1] lg:leading-[1.1] lg:text-[64px] md:text-[48px] text-[rgb(25,25,25)]'>
-            <span>
-              Start with better forms.<br></br> End with better output.
-            </span>
+        <div className='text-center col-start-1 -col-end-1 text-xl lg:col-start-3 lg:-col-end-3 md:col-start-2 md:-col-end-2 flex flex-col grid-rows-1 justify-center  items-center text-[rgb(25,25,25)] pt-4 mt-6 '>
+          <h1 className='text-5xl xl:text-[68px] xl:leading-[1] lg:leading-[1.1] lg:text-[64px] md:text-[48px] text-[rgb(25,25,25)]'>
+            <span>Forms that feel like conversations</span>
           </h1>
           <div className='text-center text-xl items-center text-[rgb(25,25,25)] pt-4 '>
             <p className='mt-8 font-normal text-none xl:text-xl xxs:text-base leading-[1.2]'>
-              Whether you need customers, feedback, or something else, Tabsform
-              helps you <br /> collect up to <strong>3.5x more data</strong>.
-              Try for free.
+              Collect data naturally. Engage <strong>users</strong> like never
+              before. Try for free.
             </p>
           </div>
           <div className='col-start-1 -col-end-1 md:justify-self-center my-10'>
@@ -22,12 +20,23 @@ export default function Product() {
               <div className='flex-col inline-flex gap-4 max-w-full'>
                 <Link
                   to='/signup'
-                  className='inline-block  cursor-pointer font-medium border-solid border-2 border-white text-black bg-white rounded-xl  py-2 px-10'
+                  className='inline-block  cursor-pointer font-medium border-solid border-2 border-black text-white bg-black rounded-xl  py-2 px-8'
                 >
                   Sign Up
                 </Link>
               </div>
             </div>
+          </div>
+        </div>
+        <BrandCarousel></BrandCarousel>
+        <div class='container'>
+          <div className='text-center text-[rgb(25,25,25)]'>
+            <div>
+              <h1 className='text-4xl xl:text-[56px] md:text-[42px] lg:text-[58px] '>
+                <span>Build forms people fill out</span>
+              </h1>
+            </div>
+            <div></div>
           </div>
         </div>
       </div>
